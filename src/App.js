b73961721f8c6a64231248a0b61f835bb9e0d3a5
EssieQ <ecauven@gmail.com>
2021-07-26 19:29:45 +1000

    installed and imported bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by Esther Cauven and is{" "}
-        <a href="https://github.com/EssieQ/my-react-weather" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by Esther Cauven and is{" "}
+          <a href="https://github.com/EssieQ/my-react-weather" target="_blank">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
