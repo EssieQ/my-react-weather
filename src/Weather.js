57d0c3835df5207405d2a48309edc5baa257c3e6
EssieQ <ecauven@gmail.com>
2021-07-27 20:41:58 +1000

    Added CSS

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,11 @@ export default function Weather() {
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,13 +27,15 @@ export default function Weather() {
         <li>Tuesday 5.25PM</li>
         <li>Sunny</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
             alt="Sunny"
           />
-          18°C
+
+          <span className="temperature">18</span>
+          <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
