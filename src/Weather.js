d1254b2ceded7173db98273a7d6458c72d888ec3
EssieQ <ecauven@gmail.com>
2021-08-03 16:44:40 +1000

    Added search form

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate";
+
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
   const [weatherState, setWeatherState] = useState({ loaded: false });
+  const [city, setCity] = useState(props.cityName);
 
   function handleResponse(response) {
     console.log(response.data);
     setWeatherState({
       loaded: true,
-      temperature: response.data.main.temp,
+      city: response.data.name,
+      temperature: Math.round(response.data.main.temp),
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
@@ -20,22 +23,31 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "bd915d97f51d3c0651893d85326bd29d";
-    let city = props.cityName;
     let units = "metric";
     let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherState.loaded) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
                 type="search"
                 placeholder="Search for a city"
                 className="form-control"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
@@ -47,30 +59,7 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <h1>{props.cityName}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={weatherState.date} />
-          </li>
-          <li>{weatherState.description}</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-              alt="Sunny"
-            />
-
-            <span className="temperature">{weatherState.temperature}</span>
-            <span className="unit">°C</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {weatherState.humidity}%</li>
-              <li>Wind: {weatherState.wind} km/h</li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data={weatherState} />
       </div>
     );
   } else {
