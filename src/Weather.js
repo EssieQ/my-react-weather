7e949844c154bb617775368e11d9b8bcffb2366d
EssieQ <ecauven@gmail.com>
2021-08-07 14:36:27 +1000

    Added forecast overview

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Weather.css";
-
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherState, setWeatherState] = useState({ loaded: false });
@@ -61,6 +61,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherState} />
+        <WeatherForecast />
       </div>
     );
   } else {
